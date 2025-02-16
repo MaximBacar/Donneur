1ff302779933cdcf94a624a908b2e7a688efb8c6
MaximBacar <maximbacar@hotmail.ca>
2025-02-15 23:42:46 -0500

    Image upload, firebase realtime db

--- donneur-backend/firebase_connector.py
+++ donneur-backend/firebase_connector.py
@@ -1,4 +1,4 @@
-from    firebase_admin          import  credentials
+from    firebase_admin          import  credentials, db
 from    firebase_admin          import  firestore
 import  firebase_admin
 
@@ -11,29 +11,57 @@ class Database:
 
     def __init__(self, firebase_credentials_path):
         firebase_credentials = credentials.Certificate(firebase_credentials_path)
-        firebase_admin.initialize_app(credential = firebase_credentials)
+        firebase_admin.initialize_app(firebase_credentials, {'databaseURL': 'https://capstone-3828a-default-rtdb.firebaseio.com/'})
 
-        self.db = firestore.client()
+        # self.db = firestore.client()
 
     
-    def create_receiver( self, fn : str, ln : str, username : str):
+    def create_receiver( self, fn : str, ln : str, username : str, id):
+        reference = db.reference('/receivers')
         data = {
             'balance'       : 0,
-            'card_id'       : None,
-            'creation_date' : datetime.now(),
+            'creation_date' : datetime.now().isoformat(),
             'first_name'    : fn,
             'last_name'     : ln,
             'username'      : username,
-            'picture_id'    : None
+            'picture_id'    : ""
         }
 
-        document = self.db.collection('Receivers').document()
-        document.set(data)
-    
-    def get_receiver( self, username ):
-        user_collection = self.db.collection('Receivers') 
-        user = user_collection.where(filter=FieldFilter("username", "==", username)).limit(1).stream()
+        reference.child(id).set(data)
+
+    def get_receiver( self, id ) -> dict:
+        reference = db.reference(f'/receivers/{id}')
+        data = reference.get()
         
-        for doc in user:
-            data = doc.to_dict()
-            print(data)
+        if data:
+            return data
+        else:
+            return None
+
+    def add_balance( self, id, amount ) -> bool:
+        reference = db.reference(f'/receivers/{id}')
+        data = reference.get()
+        if data:
+            balance = data['balance']
+            new_balance = balance + amount
+
+            reference.update({'balance' : new_balance})
+
+            return True
+        else:
+            return False
+        
+    def set_profile_picture( self, id, picture_id ):
+        reference = db.reference(f'/receivers/{id}')
+
+        data = reference.get()
+        if data:
+            reference.update({'picture_id' : picture_id})
+            return True
+
+        else:
+            return False
+
+
+
+        
