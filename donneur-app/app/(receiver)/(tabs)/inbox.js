af5e99dbbc7c00a47a476c183a2d954e529eca0b
MaximBacar <maximbacar@hotmail.ca>
2025-02-19 14:27:57 -0500

    base for inbox

--- donneur-app/app/(receiver)/(tabs)/inbox.js
+++ donneur-app/app/(receiver)/(tabs)/inbox.js
@@ -1,7 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useAuth } from '../../../context/authContext';
 
 const Inbox = () => {
+  const { user, role, loading } = useAuth();
+
+  const followedShelters = {
+    shelters:[
+         FRdseCE570TWxDGBIj0C9DY54qn2, 
+         TmTdSxjyqCfMOKJlIXmh77ZtVqD2
+    ]
+   }
+
   return (
     <View>
       <Text>inbox</Text>
